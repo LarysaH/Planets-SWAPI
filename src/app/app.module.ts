2c17e338ead6dbd1243428a7136d43dd7ba5b03e
Lara <larahumenyuk@gmail.com>
2019-11-03 00:13:07 +0100

    clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SearchBarComponent } from './search-bar/search-bar.component';
 import { PlanetListComponent } from './planet-list/planet-list.component';
 import { PlanetDetailsComponent } from './planet-details/planet-details.component';
 
@@ -15,7 +14,6 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
-    SearchBarComponent,
     PlanetListComponent,
     PlanetDetailsComponent,
     SortPipe
